f1cf196aa601847fc9f7eacae7030642d42d339f
maria <mjoop@iscte-iul.pt>
2020-05-18 18:04:05 +0100

    Classe adicionar produto e List Product

---
+++ app/src/main/java/com/example/apps/AddProductMaria.java
@@ -0,0 +1,14 @@
+package com.example.apps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddProductMaria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product_maria);
+    }
+}
